f287e5a8a0ca86ff391769d1047c00e66ada5b8f
talles <talles-guerra@hotmail.com>
2024-12-30 16:16:40 +0000

    TagItem Adjust

--- src/components/TagItem/styles.js
+++ src/components/TagItem/styles.js
@@ -1,47 +1,38 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    
+    display: flex;   
   
     background-color: ${({theme, isNew}) => isNew ? "transparent" : theme.COLORS.BACKGROUND_TAG};
-
 
     color: ${({theme}) => theme.COLORS.TEXT_LIGHT};
 
     border:  ${({theme, isNew}) => isNew ? `1px dashed ${theme.COLORS.BACKGROUND_TAG}` : "none"};
 
-
     margin: 8px 0;
     border-radius: 8px;
-  
- 
+
 
     > button{
         border: none;
         background: none;
         margin-right: 5px;
-       
-        color:  ${({theme, isNew}) => isNew ? "white" : "none"};
-
-        
+    
+        color:  ${({theme, isNew}) => isNew ? "white" : "none"};        
     }
 
     > input {
-       color: ${({theme}) => theme.COLORS.TEXT_LIGHT};
+        color: ${({theme}) => theme.COLORS.TEXT_LIGHT};
         background: transparent;
         
         margin: 0 auto;
 
-
         border: none; 
         padding: 12px;
-       
 
         &::placeholder{
             color: ${({theme}) => theme.COLORS.TEXT_INPUT}
 
         }
     }
-  
 `;
